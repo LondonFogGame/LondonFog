835cfa502bb95b5ec622b070f1f215a25cb922eb
Shafigh Bahamin <bahaminshafigh@gmail.com>
2017-09-16 18:32:49 -0600

    jump
    added jump and fog

--- LondonFog/Assets/Scripts/CharacterController.cs
+++ LondonFog/Assets/Scripts/CharacterController.cs
@@ -6,22 +6,36 @@ public class CharacterController : MonoBehaviour {
 
     public float speed;
     public float sensitivity;
-
+    public Rigidbody rb;
     // Use this for initialization
     void Start () {
-
+        speed = 5;
+        sensitivity = 10;
+        rb = GetComponent<Rigidbody>();
+       
     }
 	
 	// Update is called once per frame
-	void FixedUpdate () {
-       
+	void Update () {
+        
+
+        //smooth jumping animation
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y<=1.2)
+        {
+            rb.AddForce(transform.up * (speed - rb.velocity.y), ForceMode.Impulse);
+        }
+
+
         //Moves Forward and back along z axis                           //Up/Down
         transform.Translate(-Vector3.forward * Time.deltaTime * Input.GetAxis("Vertical") * speed);
+
         //Moves Left and right along x Axis                               //Left/Right
         transform.Translate(-Vector3.right * Time.deltaTime * Input.GetAxis("Horizontal") * speed);
 
         transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        
         if (Input.GetMouseButtonDown(0))
             Cursor.lockState = CursorLockMode.Locked;
+        
     }
 }
