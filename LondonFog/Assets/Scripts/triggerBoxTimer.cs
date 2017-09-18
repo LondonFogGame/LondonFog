e75d896af40223af9a81f903708e35626b876996
LarryNUofU <lepyzoom@live.com>
2017-09-18 01:47:24 -0600

    Added outOfBounds scene, attached script to vignette canvas, additional trigger prefab as well.
    Must drag vignette canvas object into trigger prefab script to work.

---
+++ LondonFog/Assets/Scripts/triggerBoxTimer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerBoxTimer : MonoBehaviour {
+
+	public GameObject vig;
+	triggerVignette vigScript;
+	float timer = 0f;
+	// Use this for initialization
+	void Start () {
+		vigScript = vig.GetComponent<triggerVignette> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	//void OnTriggerEnter(Collider other)
+	//{
+		//if (other.gameObject.tag.Equals("Character")
+
+	//}
+
+	void OnTriggerStay(Collider other)
+	{
+		timer += Time.deltaTime;
+		if (timer > 1.0f) {
+			vigScript.isHurting = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		vigScript.isHurting = false;
+	}
+
+}
---
+++ LondonFog/Assets/Scripts/triggerVignette.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class triggerVignette : MonoBehaviour
+{
+
+
+	public bool isHurting = false;
+
+	float countSecond = 0.0f;
+	Color col;
+	Image blackScreen;
+	// Use this for initialization
+	void Start ()
+	{
+		Image[] arr = gameObject.GetComponentsInChildren<Image> ();
+		blackScreen = arr [1];
+		col = blackScreen.color;
+		//float asdf = blackScreen.color[3];
+		col.a = 0.0f;
+		blackScreen.color = col;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+//		if (isHurting && countSecond >= 1.0f) {
+//			col[3] += 0.1f;
+//			blackScreen.color = col;
+//			countSecond = 0.0f;
+//		} else if (blackScreen.color [3] > 0.0 && countSecond >= 1.0f) {
+//			col[3] -= 0.1f;
+//			blackScreen.color = col;
+//			countSecond = 0.0f;
+//		}
+//		countSecond += Time.deltaTime;
+		if (isHurting) {
+			col.a += (0.1f * Time.deltaTime);
+			blackScreen.color = col;
+		} else if (blackScreen.color.a > 0.0f) {
+			col.a -= (0.2f * Time.deltaTime);
+			blackScreen.color = col;
+		}
+			
+
+	}
+		
+
+}
